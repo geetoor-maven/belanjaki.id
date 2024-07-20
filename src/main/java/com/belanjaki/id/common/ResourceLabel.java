c07c50c2e1582cfb59e0fc8150a757c558a7f36b
“Agus <agus40994@gmail.com>
2024-07-21 00:08:59 +0700

    Add ResourceLabel Class

--- src/main/java/com/belanjaki/id/common/ResourceLabel.java
+++ src/main/java/com/belanjaki/id/common/ResourceLabel.java
@@ -1,5 +1,18 @@
 package com.belanjaki.id.common;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:body-label.properties")
 public class ResourceLabel {
 
+    @Autowired
+    private Environment environment;
+
+    public String getBodyLabel(String key){
+        return environment.getProperty(key);
+    }
 }
